6310f4c8adafba199dfc466078bfe8052ee04f94
Alberto Medellin <albertomedellin85@gmail.com>
2020-09-17 18:06:00 -0700

    Function to upper case ready

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function upperCaseLetter (string, letter){
+    let stringArr = string.split('');
+    for(let i = 0; i<stringArr.length; i++){
+        if(stringArr[i]=== letter){
+           stringArr[i] = stringArr[i].toUpperCase();
+        }
+    }
+    const result = stringArr.join('');
     
+    return result;
 }
